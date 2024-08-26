0d0ce2f2873e91e2a77150bfb1ce0559d5072971
Devy <izabela.marcinkowska@outlook.com>
2024-08-26 17:04:19 +0200

    feat: Add rounded pictures of the flags

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -31,6 +31,8 @@ struct ContentView: View {
                         flagTapped(number)
                     } label: {
                         Image(countries[number])
+                            .clipShape(.capsule)
+                            .shadow(radius: 5)
                     }
                     
                 }
