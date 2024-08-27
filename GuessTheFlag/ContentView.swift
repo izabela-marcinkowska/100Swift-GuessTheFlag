24f3fb9eacc839eb2c851fd0935856080694ccb4
Devy <izabela.marcinkowska@outlook.com>
2024-08-27 14:34:26 +0200

    feat: Add score feature

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View {
     @State private var scoreTitle = ""
     @State private var correctAnswer = Int.random(in: 0...2)
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var score = 0
+    @State private var round = 1
     
     var body: some View {
         ZStack{
@@ -49,7 +51,7 @@ struct ContentView: View {
                 .clipShape(.rect(cornerRadius: 20))
                 Spacer()
                 Spacer()
-                Text("Score: ???")
+                Text("Score: \(score)")
                     .foregroundStyle(.white)
                     .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/.bold())
                 Spacer()
@@ -57,15 +59,17 @@ struct ContentView: View {
         }.alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Your score is ???")
+            Text("Your score is \(score)")
         }
     }
     
     func flagTapped (_ number: Int) {
         if number == correctAnswer {
-            scoreTitle = "Correct"
+            scoreTitle = "Correct! "
+            score += 1
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong. This is flag of \(countries[number])"
+            score = 0
         }
         showingScore = true
     }
